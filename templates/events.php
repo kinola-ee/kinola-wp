72430ffa0e1342dd3e6b594dae0992c6fa560dcc
Indrek Kõnnussaar <indrek@codelight.eu>
2023-08-14 19:43:55 +0300

    Fix LT translation. Add generic CSS classes to elements in templates.

--- templates/events.php
+++ templates/events.php
@@ -7,31 +7,37 @@
 
 <?php echo $rendered_filter; ?>
 <?php if ( count( $events ) ): ?>
-    <?php foreach ( $events as $event ): ?>
-        <?php /* @var $event \Kinola\KinolaWp\Event */ ?>
-        <div style="padding: 10px 20px; border: 1px solid #ccc; overflow: auto;">
-            <img src="<?php echo $event->get_field( 'production_poster' ); ?>" width="100px" height="150px"
-                 style="float: left;"/>
-            <div style="float:left; margin-left: 20px;">
-                <p>
-                    <a href="<?php echo $event->get_film_url(); ?>">
-                        <strong>
-                            <?php echo $event->get_field( 'production_title' ); ?>
-                        </strong>
-                    </a>
-                    <br>
-                    <?php echo $event->get_venue_name(); ?>
-                    <br>
-                    <?php echo $event->get_date() ?> <?php echo $event->get_time(); ?>
-                </p>
-                <p>
-                    <a href="<?php echo $event->get_checkout_url(); ?>">
-                        <?php _e( 'Buy ticket', 'kinola' ); ?>
-                    </a>
-                </p>
+    <div class="kinola-events">
+        <?php foreach ( $events as $event ): ?>
+            <?php /* @var $event \Kinola\KinolaWp\Event */ ?>
+            <div class="kinola-event" style="padding: 10px 20px; border: 1px solid #ccc; overflow: auto;">
+                <img src="<?php echo $event->get_field( 'production_poster' ); ?>" width="100px" height="150px"
+                     style="float: left;"/>
+                <div class="kinola-event-details" style="float:left; margin-left: 20px;">
+                    <p>
+                        <a class="kinola-event-title" href="<?php echo $event->get_film_url(); ?>">
+                            <strong>
+                                <?php echo $event->get_field( 'production_title' ); ?>
+                            </strong>
+                        </a>
+                        <br>
+                        <span class="kinola-event-location">
+                        <?php echo $event->get_venue_name(); ?>
+                    </span>
+                        <br>
+                        <span class="kinola-event-date">
+                        <?php echo $event->get_date() ?><?php echo $event->get_time(); ?>
+                    </span>
+                    </p>
+                    <p>
+                        <a class="kinola-event-tickets-link" href="<?php echo $event->get_checkout_url(); ?>">
+                            <?php _e( 'Buy ticket', 'kinola' ); ?>
+                        </a>
+                    </p>
+                </div>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 <?php else: ?>
     <div>
         <?php _e( 'No upcoming events!', 'kinola' ); ?>
