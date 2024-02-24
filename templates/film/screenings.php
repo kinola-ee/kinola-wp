18672d30408130a67d5d84a264b6345e4c35d80e
Indrek Kõnnussaar <indrek@indrek.it>
2024-02-24 19:49:37 +0200

    Add sold out link to screenings display of events.

--- templates/film/screenings.php
+++ templates/film/screenings.php
@@ -9,9 +9,15 @@
             <div class="kinola-film-screening">
                 <p><?php echo $event->get_venue_name(); ?></p>
                 <p><?php echo $event->get_date(); ?> <?php echo $event->get_time(); ?></p>
-                <a href="<?php echo $event->get_checkout_url(); ?>">
-                    <?php _e( 'Buy ticket', 'kinola' ); ?>
-                </a>
+                <?php if ($event->get_free_seats()): ?>
+                    <a class="kinola-screenings-tickets-link" href="<?php echo $event->get_checkout_url(); ?>">
+                        <?php _e( 'Buy ticket', 'kinola' ); ?>
+                    </a>
+                <?php else: ?>
+                    <span class="kinola-screenings-tickets-link-sold-out">
+                        <?php _e( 'Sold out', 'kinola' ); ?>
+                    </span>
+                <?php endif; ?>
             </div>
         <?php endforeach; ?>
     <?php else: ?>
