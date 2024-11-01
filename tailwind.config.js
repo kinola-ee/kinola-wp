8020350c27574cf43d568b47e29ec160d6582a88
Mairit <mairit.vikat@gmail.com>
2024-11-01 19:21:05 +0200

    Add styles to Films list

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  content: ["./templates/*.php", "./templates/*/*.php"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+        heading: ["Syncopate", "sans-serif"],
+      },
+      screens: {
+        '1xl': '1328px',
+      },
+    },
+  },
+  plugins: [],
+}
+
