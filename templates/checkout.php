5c5f01f4d12cf5985183ed2b9f9e7ea0e3693bc7
Mairit <mairit.vikat@gmail.com>
2025-06-05 17:42:19 +0300

    Add site header and footer to checkout view

--- templates/checkout.php
+++ templates/checkout.php
@@ -3,46 +3,45 @@
 /**
  * This template loads the React app which is responsible for the whole checkout process.
  */
+$template_html = get_the_block_template_html();
 ?>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title><?php echo \Kinola\KinolaWp\Checkout::get_title(); ?></title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
 
-<?php if (apply_filters('kinola/checkout/show_title', true)): ?>
-    <h1 style="font-family: Poppins; text-align: center; padding: 40px 0;">
-        <?php echo \Kinola\KinolaWp\Checkout::get_title(); ?>
-    </h1>
-<?php endif; ?>
+<div class="wp-site-blocks">
+    <?php echo do_blocks( '<!-- wp:template-part {"slug":"header","tagName":"header"} /-->' ); ?>
 
-<?php do_action('kinola/checkout/before_content'); ?>
+    <?php do_action('kinola/checkout/before_content'); ?>
 
-<div id="kinola-container"></div>
+    <div id="kinola-container"></div>
 
-<script src="<?php echo \Kinola\KinolaWp\Checkout::get_kinola_js_url(); ?>"></script>
-<script>
-    const container = document.getElementById('kinola-container')
+    <script src="<?php echo \Kinola\KinolaWp\Checkout::get_kinola_js_url(); ?>"></script>
+    <script>
+        const container = document.getElementById('kinola-container')
 
-    window.Kinola.render(container, {
-        apiBaseUrl: '<?php echo \Kinola\KinolaWp\Checkout::get_plugin_api_base_url(); ?>',
-        eventId: '<?php echo \Kinola\KinolaWp\Checkout::get_event_id(); ?>',
-        scheduleUrl: '<?php echo get_home_url(); ?>',
-        hasNewsletterCheckbox: <?php echo \Kinola\KinolaWp\Helpers::has_newsletter_checkbox() ? 'true' : 'false'; ?>,
-        newsletterCheckedByDefault: <?php echo \Kinola\KinolaWp\Helpers::newsletter_checked_by_default() ? 'true' : 'false'; ?>,
-        selectedLang: '<?php echo \Kinola\KinolaWp\Helpers::get_language(); ?>',
-        strings: <?php echo json_encode(\Kinola\KinolaWp\Checkout::get_strings()); ?>,
+        window.Kinola.render(container, {
+            apiBaseUrl: '<?php echo \Kinola\KinolaWp\Checkout::get_plugin_api_base_url(); ?>',
+            eventId: '<?php echo \Kinola\KinolaWp\Checkout::get_event_id(); ?>',
+            scheduleUrl: '<?php echo get_home_url(); ?>',
+            hasNewsletterCheckbox: <?php echo \Kinola\KinolaWp\Helpers::has_newsletter_checkbox() ? 'true' : 'false'; ?>,
+            newsletterCheckedByDefault: <?php echo \Kinola\KinolaWp\Helpers::newsletter_checked_by_default() ? 'true' : 'false'; ?>,
+            selectedLang: '<?php echo \Kinola\KinolaWp\Helpers::get_language(); ?>',
+            strings: <?php echo json_encode(\Kinola\KinolaWp\Checkout::get_strings()); ?>,
 
-        <?php if (\Kinola\KinolaWp\Helpers::get_checkout_terms_link()): ?>
-            termsLink: '<?php echo \Kinola\KinolaWp\Helpers::get_checkout_terms_link(); ?>',
-        <?php endif; ?>
-    })
-</script>
+            <?php if (\Kinola\KinolaWp\Helpers::get_checkout_terms_link()): ?>
+                termsLink: '<?php echo \Kinola\KinolaWp\Helpers::get_checkout_terms_link(); ?>',
+            <?php endif; ?>
+        })
+    </script>
 
-<?php do_action('kinola/checkout/after_content'); ?>
+    <?php do_action('kinola/checkout/after_content'); ?>
+
+    <?php echo do_blocks( '<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->' ); ?>
+</div>
 
 </body>
 </html>
